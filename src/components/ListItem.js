f1237759a32d66be9eae3a03d8b8fdf1cd3c9636
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-12-08 11:43:26 -0800

    add list item component

---
+++ src/components/ListItem.js
@@ -0,0 +1,59 @@
+import React, { useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Box, Typography, LinearProgress } from "@mui/material";
+
+import CheckOutlinedIcon from "@mui/icons-material/CheckOutlined";
+import CircleIcon from "@mui/icons-material/Circle";
+
+const ListItem = ({ id, name, todos }) => {
+  const navigate = useNavigate();
+
+  const finishedTodos = useMemo(() => {
+    const length = todos.filter((t) => t.isDone).length;
+    const percentage = (length * 100) / todos.length;
+
+    return { length, percentage };
+  }, [todos]);
+
+  const handleOnClick = () => {
+    navigate(`/list/${id}`, { state: { id, name, todos } });
+  };
+
+  const renderCheckIcon = () => {
+    if (finishedTodos.length === todos.length && todos.length > 0)
+      return <CheckOutlinedIcon color="success" />;
+
+    return <CircleIcon color="disabled" />;
+  };
+
+  return (
+    <Box
+      onClick={handleOnClick}
+      bgcolor="lightskyblue"
+      minWidth={110}
+      minHeight={140}
+      maxWidth={180}
+      maxHeight={220}
+      borderRadius={3}
+      padding={1}
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-between"
+    >
+      <Box alignSelf="flex-end">{renderCheckIcon()}</Box>
+      <Typography fontSize={12}>{name?.toUpperCase()}</Typography>
+      <Box>
+        <Typography fontSize={12} marginBottom={1}>
+          Finished {finishedTodos.length} of {todos.length}
+        </Typography>
+        <LinearProgress
+          variant="determinate"
+          value={finishedTodos.percentage || 0}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default ListItem;
