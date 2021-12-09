22b6657f45e7a78cb85238651d62a42a1ed9f500
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-12-09 14:21:58 -0800

    add redux store

---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import listsReducer from "./slices/lists";
+// import configurationReducer from './slices/configuration';
+// import authReducer from './slices/auth'
+
+const reducers = {
+  // auth: authReducer,
+  // configuration: configurationReducer,
+  lists: listsReducer,
+};
+
+const store = configureStore({
+  reducer: reducers,
+  devTools: true,
+});
+
+export default store;
