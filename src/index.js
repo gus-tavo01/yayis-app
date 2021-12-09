9983f450b5bc873c2fa7ddad38ad227ed75cb945
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-12-09 14:19:03 -0800

    add theme wrapper component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import PrivateRoute from './components/PrivateRoute';
 
+// redux store
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+import Themed from "./components/Themed";
+import { CssBaseline } from "@mui/material";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -11,18 +18,23 @@ import TodosPage from "./pages/TodosPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<ListsPage />} />
-          <Route path="list/:listId" element={<TodosPage />} />
-          {/* Login, Register, About */}
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Themed>
+        <CssBaseline />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route index element={<ListsPage />} />
+              <Route path="list/:listId" element={<TodosPage />} />
+              {/* Login, Register, About */}
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Themed>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
 
