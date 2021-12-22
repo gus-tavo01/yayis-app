5f02419773749e3eaf59c674cecf2ae77c461e52
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-12-22 12:05:50 -0800

    add default theme

---
+++ src/constants/defaults.js
@@ -0,0 +1,23 @@
+import { colors } from "@mui/material";
+
+export const theme = {
+  code: "T_DEF",
+  name: "Default theme",
+  palette: {
+    type: "light",
+    primary: {
+      main: colors.purple.A700,
+      text: "white",
+    },
+    secondary: {
+      main: colors.lightBlue.A200,
+      text: "black",
+    },
+  },
+};
+
+export const language = { code: "L_SPA", name: "Spanish" };
+
+const defaults = { language, theme };
+
+export default defaults;
