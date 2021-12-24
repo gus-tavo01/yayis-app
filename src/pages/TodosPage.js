70d3fbdfa4c689f483d327b6b773751fc8703ceb
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-12-24 14:11:29 -0800

    add temporal handlers for update, delete actions

--- src/pages/TodosPage.js
+++ src/pages/TodosPage.js
@@ -1,18 +1,89 @@
-import React from "react";
-import { Grid, Typography } from "@mui/material";
-import { useParams } from "react-router";
+import React, { useState } from "react";
+
+import { useLocation } from "react-router";
+import { useSelector } from "react-redux";
+
+import { Box, Fab, LinearProgress } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import AddIcon from "@mui/icons-material/Add";
+
+import TodoItem from "../components/TodoItem";
+import AddTodoModal from "../components/modals/AddTodoModal";
+
+const useStyles = makeStyles((theme) => ({
+  fab: {
+    position: "fixed",
+    // top: theme.spacing(6),
+    // right: theme.spacing(1),
+  },
+}));
 
 const TodosPage = () => {
-  const params = useParams();
+  const classes = useStyles();
+  const { state: currentList } = useLocation();
+
+  const [addTodoOpen, setAddTodoOpen] = useState(false);
+
+  // TODO:
+  // const todos = useSelector((store) => store.todos);
+  const todos = { loading: false };
+
+  const renderTodos = () => {
+    return [{ id: "asdad", name: "test", isDone: true }].map((t) => (
+      <TodoItem
+        key={t.id}
+        id={t.id}
+        name={t.name}
+        isDone={t.isDone}
+        description={t.description}
+        onUpdate={handleOnUpdate}
+        onDelete={handleOnDelete}
+      />
+    ));
+  };
+
+  const handleOnCreate = (newTodo) => {
+    // TODO
+    // dispatch(createTodo(newTodo));
+    console.log("## on TODO submit", newTodo);
+  };
+
+  const handleOnUpdate = (todoId, update) => {
+    // TODO
+    // dispatch(updateTodo(listId, todoId, update));
+    console.log("# will update ", todoId, update);
+  };
+
+  const handleOnDelete = (todoId) => {
+    // TODO
+    // dispatch(removeTodo(listId, todoId));
+    console.log("# will remove ", todoId);
+    console.log("from ", currentList.id);
+  };
 
   console.log("@@ Todos page render");
 
   return (
-    <Grid>
-      <Typography variant="h4" component="h2">
-        Todos from list {params.listId}
-      </Typography>
-    </Grid>
+    <Box>
+      {todos.loading && <LinearProgress />}
+      {renderTodos()}
+
+      <AddTodoModal
+        open={addTodoOpen}
+        onCancel={() => setAddTodoOpen(false)}
+        onSubmit={handleOnCreate}
+      />
+
+      <Box display="flex" justifyContent="flex-end">
+        <Fab
+          onClick={() => setAddTodoOpen(true)}
+          color="primary"
+          className={classes.fab}
+        >
+          <AddIcon />
+        </Fab>
+      </Box>
+    </Box>
   );
 };
 
