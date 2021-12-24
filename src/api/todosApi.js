a6cc84703474618feab48e71f89bec519e93b51a
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-12-24 14:13:04 -0800

    add TODOS api configuration

--- src/api/todosApi.js
+++ src/api/todosApi.js
@@ -8,29 +8,17 @@ const errorCb = (error) => {
 
   const payload = error.response?.data || errorPayload;
 
-  console.log("# on axios error CB");
+  console.log("## axios error CB");
 
   return payload;
 };
 
 const api = axios.create({
-  baseURL: "apiUrl", // TODO: get from env
+  baseURL: process.env.REACT_APP_TODOS_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 api.interceptors.response.use(successCb, errorCb);
 
-// export default api;
-
-const fakeApi = {
-  get: async (params) => [
-    { id: "333ll", name: "Test", description: "Test description", todos: [] },
-    { id: "444", name: "Cena", description: "Test description", todos: [] },
-    { id: "555", name: "Comida", description: "Test description", todos: [] },
-    { id: "666", name: "Desayuno", description: "Test description", todos: [] },
-    { id: "777", name: "Postre", description: "Test description", todos: [] },
-  ],
-};
-
-export default fakeApi;
+export default api;
