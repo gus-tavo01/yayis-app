956c26257cf8e9bba89d56437c165d62d3ccb25c
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-12-29 22:25:04 -0800

    remove service

--- src/services/todosService.js
+++
@@ -1,8 +0,0 @@
-import todosApi from "../api/todosApi";
-
-export const getLists = (params) => todosApi.get("/lists", { params });
-export const postList = (list) => todosApi.post("/lists", list);
-
-const todosService = { getLists, postList };
-
-export default todosService;
