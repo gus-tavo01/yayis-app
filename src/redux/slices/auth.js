ad972af9cfe3e8ff872bdf74cb6aff928c9044d6
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-12-29 16:02:06 -0800

    add temporal auth slice

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,14 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import authService from '../../services/authService';
+// import usersService from '../../services/usersService';
 
 const initialState = {
+  loading: false,
   user: null,
-  loading: true,
 };
+
+// const login = createAsyncThunk
+
+const fetchProfile = createAsyncThunk(
+  "fetch/profile",
+  async ({ token, email }, { dispatch }) => {
+    // dispatch(setLoading(true));
+    // const response = await usersService.getProfile(token, email);
+    // dispatch(setLoading(false));
+    // return response?.payload;
+  }
+);
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+  },
+  extraReducers: {
+    [fetchProfile.fulfilled]: (state, action) => {
+      state.user = action.payload;
+    },
+  },
 });
 
+export const { setLoading } = authSlice.actions;
 export default authSlice.reducer;
