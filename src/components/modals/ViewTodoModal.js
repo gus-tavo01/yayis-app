ebf5265fe6c93f659cd209443277bc12cb377067
gus-tavo01 <gustavoa.loera01@gmail.com>
2022-01-03 15:19:26 -0800

    Add modal component view todo

---
+++ src/components/modals/ViewTodoModal.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  DialogContentText,
+} from "@mui/material";
+
+const ViewTodoModal = ({ open, onClose, todo }) => {
+  return (
+    <Dialog open={open} fullWidth>
+      <DialogTitle>{todo.name}</DialogTitle>
+      <DialogContent dividers>
+        <DialogContentText style={{ whiteSpace: "pre-line" }}>
+          {todo.description}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions sx={{ display: "flex", justifyContent: "center" }}>
+        <Button onClick={() => onClose()} color="primary" variant="contained">
+          Ok
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ViewTodoModal;
