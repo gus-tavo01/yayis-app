cf195d42ef0bf1805ad07f25e2bfdc60eaccac30
gus-tavo01 <gustavoa.loera01@gmail.com>
2022-01-04 10:41:10 -0800

    add pending auth slice

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -2,34 +2,47 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 // import authService from '../../services/authService';
 // import usersService from '../../services/usersService';
 
-const initialState = {
-  loading: false,
-  user: null,
-};
-
-// const login = createAsyncThunk
-
-const fetchProfile = createAsyncThunk(
-  "fetch/profile",
-  async ({ token, email }, { dispatch }) => {
+const login = createAsyncThunk(
+  "post/login",
+  async (credentials, { dispatch }) => {
     // dispatch(setLoading(true));
-    // const response = await usersService.getProfile(token, email);
+    // const response await authService.login(credentials);
+    // if (response.errorMessage) {
+    //   // dispatch(error);
+    //   return null;
+    // }
+    // localStorage.setItem("authToken", response?.payload?.token);
+    // usersService.setToken(token);
+    // const getUserResponse = await usersService.get(credentials.email);
+    // if (getUserResponse.errorMessage) { dispatch(error); return null; }
     // dispatch(setLoading(false));
-    // return response?.payload;
+    // return getUserResponse?.payload;
   }
 );
 
+const logout = createAsyncThunk("", async ({ dispatch }) => {
+  // dispatch(setLoading(true));
+  // localStorage.removeItem("authToken");
+  // dispatch(setLoading(false));
+});
+
 const authSlice = createSlice({
   name: "auth",
-  initialState,
+  initialState: {
+    loading: false,
+    user: null,
+  },
   reducers: {
     setLoading: (state, action) => {
       state.loading = action.payload;
     },
   },
   extraReducers: {
-    [fetchProfile.fulfilled]: (state, action) => {
+    [login.fulfilled]: (state, action) => {
       state.user = action.payload;
+    },
+    [logout.fulfilled]: (state, action) => {
+      state.user = null;
     },
   },
 });
