4cab3c30c5e9ece91700886c76500a234c32bc08
gus-tavo01 <gustavoa.loera01@gmail.com>
2022-01-04 10:22:03 -0800

    add behavior for toggling description

--- src/components/modals/AddTodoModal.js
+++ src/components/modals/AddTodoModal.js
@@ -7,10 +7,14 @@ import {
   DialogActions,
   InputLabel,
   Button,
+  Divider,
 } from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 const AddTodoModal = ({ open, onCancel, onSubmit }) => {
   const [inputs, setInputs] = useState({ name: "", description: "" });
+  const [hiddenDescription, setHiddenDescription] = useState(true);
 
   const handleOnChange = ({ target }) =>
     setInputs({ ...inputs, [target.name]: target.value });
@@ -38,14 +42,29 @@ const AddTodoModal = ({ open, onCancel, onSubmit }) => {
           onChange={handleOnChange}
           value={inputs.name}
         />
-        <InputLabel>Description</InputLabel>
-        <TextField
-          name="description"
-          placeholder="Todo detailed description"
-          onChange={handleOnChange}
-          value={inputs.description}
-          multiline
-        />
+        <InputLabel
+          onClick={() => setHiddenDescription(!hiddenDescription)}
+          sx={{ display: "flex", alignItems: "center", marginTop: 1 }}
+        >
+          Description
+          {!hiddenDescription ? (
+            <KeyboardArrowDownIcon color="inherit" />
+          ) : (
+            <KeyboardArrowUpIcon color="inherit" />
+          )}
+        </InputLabel>
+
+        {!hiddenDescription && <Divider />}
+
+        {hiddenDescription && (
+          <TextField
+            name="description"
+            placeholder="Todo detailed description"
+            onChange={handleOnChange}
+            value={inputs.description}
+            multiline
+          />
+        )}
       </DialogContent>
       <DialogActions sx={{ display: "flex", justifyContent: "space-between" }}>
         <Button onClick={handleOnCancel} color="secondary" variant="outlined">
