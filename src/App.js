42b9ce9dda3ed8e66e89fe804d1370b5989fa03d
gus-tavo01 <gustavoa.loera01@gmail.com>
2022-01-11 11:31:21 -0800

    add loading at app level

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { Outlet, useLocation } from "react-router-dom";
 import AppNav from "./components/AppNav";
 import TodosNav from "./components/TodosNav";
 import ToastBar from "./components/ToastBar";
+import { LinearProgress } from "@mui/material";
+
+import { useSelector } from "react-redux";
 
 const App = () => {
   const location = useLocation();
+  const authLoading = useSelector((store) => store.auth.loading);
   const isTodosPage = location.pathname.includes("/list/");
 
   console.log("@@ App render");
@@ -14,6 +18,7 @@ const App = () => {
   return (
     <>
       {!isTodosPage ? <AppNav /> : <TodosNav />}
+      {authLoading && <LinearProgress color="secondary" />}
       <Outlet />
       <ToastBar />
     </>
