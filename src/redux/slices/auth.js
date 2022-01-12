fbac6be6eb1424d4b84d8fe34fd8709f945da1ba
gus-tavo01 <gustavoa.loera01@gmail.com>
2022-01-12 13:48:46 -0800

    Reducer implement login error handling

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -2,35 +2,52 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import authService from "../../services/authService";
 import usersService from "../../services/usersService";
 
+import { setLoading as setAlertsLoading } from "./alerts";
+
 export const login = createAsyncThunk(
   "auth/login",
   async (credentials, { dispatch }) => {
     dispatch(setLoading(true));
     const response = await authService.login(credentials);
-    // TODO
-    // if (response.errorMessage) {
-    //   // dispatch(error);
-    //   return null;
-    // }
+
+    if (response.statusMessage === "Unauthorized") {
+      dispatch(setError("El correo o la contraseña no son validos."));
+      return null;
+    }
+
+    if (response.errorMessage) {
+      dispatch(setError("Algo salio mal, vuelve a intentarlo mas tarde"));
+      return null;
+    }
+
     const { token } = response?.payload;
     localStorage.setItem("authToken", token);
     usersService.setToken(token);
 
     const getUserParams = { email: credentials.email };
     const getProfileResponse = await usersService.get(getUserParams);
-    // TODO
-    // if (getProfileResponse.errorMessage) { dispatch(error); return null; }
-    const [user] = getProfileResponse.payload?.docs;
-    dispatch(setLoading(false));
-    return user || null;
+
+    if (getProfileResponse.errorMessage) {
+      dispatch(setError("Algo salio mal, vuelve a intentarlo mas tarde"));
+      return null;
+    }
+
+    const [user = null] = getProfileResponse.payload?.docs;
+    return user;
   }
 );
 
 export const logout = createAsyncThunk(
   "auth/logout",
   async (logout, { dispatch }) => {
-    dispatch(setLoading(true));
+    dispatch(setAlertsLoading(true));
     localStorage.removeItem("authToken");
+
+    // TODO
+    // clean up user data
+    // lists
+    // configuration
+    dispatch(setAlertsLoading(false));
   }
 );
 
@@ -39,22 +56,26 @@ const authSlice = createSlice({
   initialState: {
     loading: false,
     user: null,
+    error: null,
   },
   reducers: {
     setLoading: (state, action) => {
       state.loading = action.payload;
     },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
   },
   extraReducers: {
     [login.fulfilled]: (state, action) => {
       state.user = action.payload;
+      state.loading = false;
     },
     [logout.fulfilled]: (state) => {
       state.user = null;
-      state.loading = false;
     },
   },
 });
 
-export const { setLoading } = authSlice.actions;
+export const { setLoading, setError } = authSlice.actions;
 export default authSlice.reducer;
