922763e0ef74dab9f6d2faa72c69b93cd2ef7fdf
gus-tavo01 <gustavoa.loera01@gmail.com>
2022-01-19 12:18:41 -0800

    add about page content

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,23 @@
 import React from "react";
 
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const AboutPage = () => {
-  return <Box>Made with ❤️ by GALV</Box>;
+  return (
+    <Box p={1}>
+      <Typography variant="h4" component="h2">
+        Equipo de desarrollo:
+      </Typography>
+      <ul>
+        <li>Ticky (Project manager)</li>
+        <li>Yayis (Product owner)</li>
+        <li>Gustavo Loera (Main Developer)</li>
+        <li>Nancy Sarahi (Ideas)</li>
+      </ul>
+      <Typography variant="caption">Version 1.1.0</Typography>
+      <Typography>Made with ❤️ by GALV</Typography>
+    </Box>
+  );
 };
 
 export default AboutPage;
