d7cccde75806c69799eaf44e2c82b41ab52c0036
gus-tavo01 <gustavoa.loera01@gmail.com>
2022-01-21 12:28:13 -0800

    auth service add register method

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,6 +3,9 @@ import todosApi from "../api/todosApi";
 export const login = async (credentials) =>
   todosApi.post(`/v1/users/login`, credentials);
 
-const authService = { login };
+export const register = async (email, password) =>
+  todosApi.post(`/v1/users`, { email, password });
+
+const authService = { login, register };
 
 export default authService;
