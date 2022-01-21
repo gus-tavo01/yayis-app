5135c9649790411e6e5eaa84e3e5af586698fbd5
gus-tavo01 <gustavoa.loera01@gmail.com>
2022-01-21 13:39:33 -0800

    add Todos CDU

--- src/services/listsService.js
+++ src/services/listsService.js
@@ -22,6 +22,27 @@ export const update = async (userId, listId, list) => {
   return todosApi.patch(`/v1/users/${userId}/lists/${listId}`, list);
 };
 
-const listsService = { get, create, update, remove, setToken };
+export const addTodo = async (listId, todo) => {
+  return todosApi.post(`/v1/lists/${listId}/todos`, todo);
+};
+
+export const removeTodo = async (listId, todoId) => {
+  return todosApi.delete(`/v1/lists/${listId}/todos/${todoId}`);
+};
+
+export const updateTodo = async (listId, todoId, patch) => {
+  return todosApi.patch(`/v1/lists/${listId}/todos/${todoId}`, patch);
+};
+
+const listsService = {
+  get,
+  create,
+  update,
+  remove,
+  setToken,
+  addTodo,
+  removeTodo,
+  updateTodo,
+};
 
 export default listsService;
