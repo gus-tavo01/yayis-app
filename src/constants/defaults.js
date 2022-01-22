35c95b39fd4b972a67a7bfc6fd2c08b9cef7dc6b
gus-tavo01 <gustavoa.loera01@gmail.com>
2022-01-22 15:09:35 -0800

    update defaults file

--- src/constants/defaults.js
+++ src/constants/defaults.js
@@ -1,22 +1,5 @@
-import { colors } from "@mui/material";
-
-export const theme = {
-  code: "T_DEF",
-  name: "Default theme",
-  palette: {
-    type: "light",
-    primary: {
-      main: colors.purple.A700,
-      text: "white",
-    },
-    secondary: {
-      main: colors.lightBlue.A200,
-      text: "black",
-    },
-  },
-};
-
-export const language = { code: "L_SPA", name: "Español" };
+export const theme = "T_DEF";
+export const language = "L_SPA";
 
 export const pwdMinLength = 6;
 
